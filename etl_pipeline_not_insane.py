a70431dac8cfaaae64f60f0ab0f2c7ff1411824a
CarthusSandworm <ahaubrich1@gmail.com>
2026-06-17 16:26:16 -0400

    added flexible input, testing for null keys, comment documentation, default values, replacements

--- etl_pipeline_not_insane.py
+++ etl_pipeline_not_insane.py
@@ -1,5 +1,7 @@
+from asyncio.windows_events import NULL
 import json
 from pathlib import Path
+from numpy import NaN
 import psycopg
 # from psycopg import sql
 from psycopg.errors import UniqueViolation
@@ -28,60 +30,92 @@ def import_file(file_name):
                 port="5432"
             )
     except:
+
+        # Include a fallback in case I'm stupid and forget to start the postgres server
         print("Connection not found!")
         valid_connection = 0
 
+    # Execute only if the connection is established
     if valid_connection != 0:
         with connection.cursor() as cur:
-
+            
+            # Creates a
             cur.execute("""
                 CREATE TABLE IF NOT EXISTS user_profiles (
-                    name TEXT,
-                    language TEXT,
+                    name TEXT DEFAULT NULL,
+                    language TEXT DEFAULT NULL,
                     id VARCHAR(16) PRIMARY KEY,
-                    bio TEXT,
-                    version FLOAT
+                    bio TEXT DEFAULT NULL,
+                    version FLOAT DEFAULT NULL
                 );
             """)
 
             with open(working_file, "r") as read_file:
 
                 if file_extension == "json":
-
+                    
                     # Open json file 
                     json_file = json.load(read_file)
 
+                    # Create format to insert users
                     insert_user = "INSERT INTO user_profiles (name, language, id, bio, version) VALUES (%s, %s, %s, %s, %s);"
 
+                    # Loop through all users and insert any non-duplicates with a valid ID
                     for user in json_file:
-                        try:
-                            cur.execute(insert_user, (user["name"], user["language"], user["id"], user["bio"], user["version"]))
-                        except UniqueViolation as e:
-                            print(f"Error: That user ID already exists. ({e})")
+                        
+                        # Set defaults for all fields then replace with values 
+                        name, language, id, bio, version = NULL, NULL, NULL, NULL, NULL
+
+                        if "name" in user:
+                            name = user["name"]
+                        if "language" in user:
+                            language = user["language"]
+                        if "id" in user:
+                            id = user["id"]
+                        if "bio" in user:
+                            bio = user["bio"]
+                        if "version" in user:
+                            version = user["version"]
+
+                        # For all non-null values, insert the entry into the table
+                        if id != NULL:
+                            try:
+                                cur.execute(insert_user, (name, language, id, bio, version))
+                            except UniqueViolation as e:
+                                print(f"Error: User ID {id} already exists. ({e})")
+                        else:
+                            print("Skipping row with missing ID.")
 
                         connection.commit()
                     
                 elif file_extension == "csv":
 
-                    print("meme currently under construction, please wait")
-                    print("shoplifters when the shop is too heavy")
-
-                    pd.options.display.max_rows = 25
-
+                    # Convert the CSV file into a Pandas DataFrame
                     csv_df = pd.read_csv(read_file)
 
-                    csv_records = csv_df.to_records(index = False)
+                    # Set default values for all columns of the DataFrame
+                    csv_df = csv_df.replace(NaN, NULL)
+                    csv_df_fill = csv_df.fillna({"name": NULL, "language": NULL, "id": NULL, "bio": NULL, "version": NULL})
+
+                    # Transform above DataFrame into a NumPy array
+                    csv_records = csv_df_fill.to_records(index = False)
                     
+                    # For each entry in the CSV file, ensure the ID column is not empty, and if so attempt to insert. The only exception will be if the user's ID is already present.
                     for row in csv_records:
-                        try:
-                            cur.execute("""
-                                INSERT INTO user_profiles (name, language, id, bio, version) VALUES (%s, %s, %s, %s, %s);
-                            """, tuple(row)) 
-                        except UniqueViolation as e:
-                            print(f"Error: That user ID already exists. ({e})")
+                        if row[2] != NULL:
+                            try:
+                                cur.execute("""
+                                    INSERT INTO user_profiles (name, language, id, bio, version) VALUES (%s, %s, %s, %s, %s);
+                                """, tuple(row)) 
+                            except UniqueViolation as e:
+                                print(f"Error: That user ID already exists. ({e})")
+                        else:
+                            print("Skipping row with missing ID.")
+
+                        connection.commit()
 
             cur.close()
             connection.close()
 
-import_file("single_example.csv")
-# import_file("first5.json") 
+# import_file("single_example.csv")
+# import_file("first5.json")
